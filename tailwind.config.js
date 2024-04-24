72c729821415735a82878969b4e88a6d3c963172
saikat cfx <saikatxyz738@gmail.com>
2024-04-24 23:14:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        sm:"0.75rem",
+        bas:"0.8rem",
+       
+
+
+
+      }
+    },
+  },
+  plugins: [],
+}
+
